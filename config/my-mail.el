e12e3dcf603d6dd3e17aaa88d8fc0d51b7da2bd8
Gökhan Kıcı <gokhankici@gmail.com>
2014-03-05 22:12:28 +0200

    mail functionality is added to emacs with mu4e & offlineimap

---
+++ config/my-mail.el
@@ -0,0 +1,94 @@
+;; ------------------------------------------------------------------------
+;; mail settings
+;; ------------------------------------------------------------------------
+
+(require 'mu4e)
+(require 'smtpmail)
+
+;;; list the mail accounts here
+(defvar my-mu4e-account-alist
+  '(("Gmail"
+     ( mu4e-drafts-folder "/Gmail/[Gmail].Drafts" )
+     ( mu4e-sent-folder "/Gmail/[Gmail].Sent Mail" )
+     ( mu4e-trash-folder "/Gmail/[Gmail].Trash" )
+     ( user-mail-address "gokhankici@gmail.com" )
+     ( user-full-name "Gökhan Kıcı" )
+     ( message-signature "Gokhan" )
+     ( smtpmail-default-smtp-server "smtp.gmail.com" )
+     ( smtpmail-smtp-server "smtp.gmail.com" )
+     ( smtpmail-smtp-service 587 ))
+    ("Boun"
+     ( mu4e-drafts-folder "/Boun/Taslaklar" )
+     ( mu4e-sent-folder "/Boun/Sent" )
+     ( mu4e-trash-folder "/Boun/Trash" )
+     ( user-mail-address "gokhan.kici@boun.edu.tr" )
+     ( user-full-name "Gökhan Kıcı" )
+     ( message-signature "Gokhan" )
+     ( message-send-mail-function 'smtpmail-send-it )
+     ( smtpmail-stream-type 'starttls )
+     ( smtpmail-default-smtp-server "smtp.boun.edu.tr" )
+     ( smtpmail-smtp-server "smtp.boun.edu.tr" )
+     ( smtpmail-smtp-service 587 ))))
+
+;;; Set the default mail account
+(setq my-mail-default-mail-account "Gmail")
+
+;;; Load the default mail account's parameters
+(let ((account-vars 
+       (cdr (assoc my-mail-default-mail-account my-mu4e-account-alist))))
+  (mapc #'(lambda (var) (set (car var) (cadr var))) account-vars))
+
+;;; Set the default mail account's shortcuts
+(defun my-mail-set-shortcuts (account)
+  (if (string= account "Gmail")
+      (setq mu4e-maildir-shortcuts '( ("/Gmail/INBOX"               . ?i)
+                                      ("/Gmail/[Gmail].Sent Mail"   . ?s)
+                                      ("/Gmail/[Gmail].Trash"       . ?t)
+                                      ("/Gmail/[Gmail].All Mail"    . ?a))
+            message-send-mail-function 'smtpmail-send-it
+            smtpmail-stream-type 'starttls)
+    (setq mu4e-maildir-shortcuts '( ("/Boun/INBOX"  . ?i)
+                                    ("/Boun/Sent"   . ?s)
+                                    ("/Boun/Trash"  . ?t))
+          message-send-mail-function 'smtpmail-send-it
+          smtpmail-stream-type 'starttls)))
+
+(my-mail-set-shortcuts "Gmail")
+
+;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+(setq mu4e-sent-messages-behavior 'delete)
+
+;; allow for updating mail using 'U' in the main view:
+(setq mu4e-get-mail-command "offlineimap")
+
+;; don't keep message buffers around
+(setq message-kill-buffer-on-exit t)
+
+;;; view html files
+(setq mu4e-html2text-command "html2text -utf8 -width 72")
+
+(setq mu4e-view-show-images t)
+
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message."
+  (interactive)
+  (let* ((account
+          (if mu4e-compose-parent-message
+              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                (string-match "/\\(.*?\\)/" maildir)
+                (match-string 1 maildir))
+            (completing-read (format "Compose with account: (%s) "
+                                     (mapconcat #'(lambda (var) (car var)) my-mu4e-account-alist "/"))
+                             (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                             nil t nil nil (caar my-mu4e-account-alist))))
+         (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+        (progn 
+          (mapc #'(lambda (var) (set (car var) (cadr var))) account-vars) 
+          (my-mail-set-shortcuts account))
+      (error "No email account found"))))
+
+;;; check for current mail settings when sending an email
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+
+(provide 'my-mail)
--- init.el
+++ init.el
@@ -68,7 +68,7 @@
                            my-clock
                            my-dictionary
                            my-functions
-                           ;; my-color
+                           my-mail
                            ))
 
 
@@ -256,47 +256,3 @@ to your recently and most frequently used commands.")
 
 (add-hook 'term-mode-hook               ; enable tab support for ansi-term
           (lambda() (setq yas-dont-activate t)))
-
-
-;; ------------------------------------------------------------------------
-;; mail settings
-;; ------------------------------------------------------------------------
-(require 'mu4e)
-
-;; default
-(setq mu4e-maildir "~/Maildir")
-(setq mu4e-drafts-folder "/[Gmail].Drafts")
-(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
-(setq mu4e-trash-folder  "/[Gmail].Trash")
-
-;; don't save message to Sent Messages, Gmail/IMAP takes care of this
-(setq mu4e-sent-messages-behavior 'delete)
-
-;; setup some handy shortcuts
-;; you can quickly switch to your Inbox -- press ``ji''
-;; then, when you want archive some messages, move them to
-;; the 'All Mail' folder by pressing ``ma''.
-(setq mu4e-maildir-shortcuts
-      '( ("/INBOX"               . ?i)
-         ("/[Gmail].Sent Mail"   . ?s)
-         ("/[Gmail].Trash"       . ?t)
-         ("/[Gmail].All Mail"    . ?a)))
-
-;; allow for updating mail using 'U' in the main view:
-(setq mu4e-get-mail-command "offlineimap")
-
-;; something about ourselves
-(setq
- user-mail-address "gokhankici@gmail.com"
- user-full-name  "Gökhan Kıcı"
- message-signature  "Gokhan")
-
-(require 'smtpmail)
-(setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-stream-type 'starttls
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587)
-
-;; don't keep message buffers around
-(setq message-kill-buffer-on-exit t)
